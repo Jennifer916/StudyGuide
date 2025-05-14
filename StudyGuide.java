b1389e9a61b7fca49bd3a3f0e668098455937514
Jennifer916 <jenniferchenxil2789@nycstudents.net>
2025-05-14 16:33:57 +0000

    2nd change

--- StudyGuide.java
+++ StudyGuide.java
@@ -1,7 +1,55 @@
 public class StudyGuide {
-    private int [] scores1; 
-    private int [] scores2; 
-    private int [] scores3; 
-    
+    private ArrayList <Integer> scores;
+    private String mode;
+    private String answer; 
+    private ArrayList <String [] [] > questions; 
+    public StudyGuide (String m) {
+        mode = m; 
+    }
+    public String playgame() {
+        int score = 0 ; 
+        int miss= 0 ; 
+        int count = 0 ;
+        if (mode = "hard") {
+            miss = 3; 
+        }
+        while (count>miss) {
+                int random = (int) (Math.random () * questions.size()) + 1; 
+                getanswer();
+                if (questions.get(random)[2]= getanswer()) {
+                    score++;
+                } else {
+                    count++;
+                }
+            }
+          scores.add(score); 
+          return  "Congradulations, you got a " + score;  
+    }
+    public String getquestion () {
+        Scanner s = new Scanner (System.in) ;
+        answer = s.nextline();
+        return answer;  
+    }
+    public ArrayList <String> questionbank () {
+        return questions;
+    }
+    public void change ( String question, String answer) {
+        
+    }
+    public boolean modifyquestion (int index, String question, String answer) {
+        String [][] arr = {question, answer};
+        if (index>questions.size ()) {
+            return false; 
+        } else { 
+            if (index==question.size() ) {
+                questions.add(arr);
+                return true;
+            }
+        }
+         questions.set(index,arr);
+         return true; 
+        
+        
+    }
 
 }
