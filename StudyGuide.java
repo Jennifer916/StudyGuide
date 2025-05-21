7bbab04c47ae08fb46f3a214789d89e5d19bb71d
BT_1N3_03 <jenniferchenxil2789@nystudents.net>
2025-05-21 12:20:18 -0400

    4th change

--- StudyGuide.java
+++ StudyGuide.java
@@ -1,66 +1,64 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 public class StudyGuide {
-    private ArrayList <Integer> scores;
+    private int highscore;
     private String mode;
-    private String answer; 
-    private ArrayList <String[]> questions;
-  //  public StudyGuide () {
-        
-  //  }
-    public String playgame() {
-        int score = 0 ; 
-        int miss= 3 ; 
-        int count = 0 ;
-        while (count>miss) {
-                if (getquestion()) {
-                    score++:
-                } else {
-                    count++;
-                }
-        }
-          sortscores(score); 
-          return  "Congradulations, you got a " + score;  
+    private String answer;
+    private ArrayList<String[]> questions;
+    public StudyGuide () {
+        questions = new ArrayList<String[]> ();
     }
-    public boolean getquestion () {
-        int random = (int) (Math.random () * questions.size() + 1);
-        System.out.println(questions.get(random)[1]);
-        Scanner s = new Scanner (System.in);
-        String answer = s.nextLine();
-        if (answer.indexOf(questions.get(random)[2])>=0 ) {
-            score++;
-            return true; 
+    public String playgame() {
+        int score = 0;
+        int miss = 3;
+        int count = 0;
+        while (count > miss) {
+            if (getquestion()) {
+                score++;
+            } else {
+                count++;
+            }
         }
-        count++;
+        if (score > highscore) {
+            highscore = score;
+        }
+        return "Congradulations, you got a " + score;
+    }
+
+    public boolean getquestion() {
+        int random = (int) (Math.random() * questions.size() + 1);
+        System.out.println(questions.get(random)[1]);
+        Scanner s = new Scanner(System.in);
+        String answer = s.nextLine();
+        if (answer.indexOf(questions.get(random)[2]) >= 0) {
+
+            return true;
+        }
+
         return false;
     }
-    public ArrayList <String[]> questionbank () {
+
+    public ArrayList<String[]> questionbank() {
         return questions;
     }
-    public void modifyquestion (int index, String question, String answer) {
-        String [] arr = {question, answer};
-        if (index>=questions.size ()) {
-                questions.add(arr);
-            }
-         questions.set(index,arr);  
+
+    public void modifyquestion(int index, String question, String answer) {
+        String[] arr = {question, answer};
+        if (index >= questions.size()) {
+            questions.add(arr);
+        }
+        questions.set(index, arr);
     }
-    public void addquestion (String question, String answer) {
-        String [] list = {question, answer};
+
+    public void addquestion(String question, String answer) {
+        String[] list = {question, answer};
         questions.add(list);
-    } 
-    public void sortscores (int score) {
-            for (int i = 0 ; i<scores.size() ; i++ ) {
-                if (score<scores.get(i)) {
-                    scores.add(i, score);
-                }
-            }
-        scores.add(score) ; 
-}
+    }
+
+
     public int getscore() {
-        return scores;
+        return highscore;
     }
-     public int getspecscore(int index) {
-        return scores[index];
-    }
+
 }
     
